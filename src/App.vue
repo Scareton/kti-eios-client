79a7fa851c242c8f8bbe4c8ec56bcee6b373f23f
Sundear <voron4288@gmail.com>
2020-04-26 00:55:03 +0400

    navigation v1

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,54 @@
 <template>
   <v-app>
     <v-navigation-drawer v-model="drawer" :mini-variant="drawerMiniVariant" :expand-on-hover="drawerExpandOnHover" app>
-      <v-list dense nav class="pa-0">
-        <v-list-item two-line :class="miniVariant && 'px-0'">
+      <!-- Links -->
+      <v-list dense nav class="py-0">
+        <v-list-item two-line :class="drawerMiniVariant && 'px-0'">
           <v-list-item-avatar>
             <img src="https://randomuser.me/api/portraits/men/81.jpg" />
           </v-list-item-avatar>
 
           <v-list-item-content>
-            <v-list-item-title>Application</v-list-item-title>
-            <v-list-item-subtitle>Subtext</v-list-item-subtitle>
+            <v-list-item-title>Александр Воронков</v-list-item-title>
+            <v-list-item-subtitle>Студент 4-го курса</v-list-item-subtitle>
+            <v-list-item-subtitle>Информационные системы (по отраслям)</v-list-item-subtitle>
           </v-list-item-content>
         </v-list-item>
 
-        <v-divider></v-divider>
+        <v-divider class="mb-4" />
 
-        <v-list-item v-for="item in items" :key="item.title" link>
+        <v-list-item v-for="link in links" :key="link.title" :to="link.url" link>
           <v-list-item-icon>
-            <v-icon>{{ item.icon }}</v-icon>
+            <v-icon>{{ link.icon }}</v-icon>
           </v-list-item-icon>
 
           <v-list-item-content>
-            <v-list-item-title>{{ item.title }}</v-list-item-title>
+            <v-list-item-title>{{ link.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+
+      <!-- base links (profile etc.) -->
+      <v-divider class="my-4" />
+      <v-list dense nav class="py-0">
+        <!-- if user -->
+        <v-list-item link to="/profile">
+          <v-list-item-icon>
+            <v-icon>mdi-account</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>Личный кабинет</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <!-- exit -->
+        <v-list-item link to="/logout">
+          <v-list-item-icon>
+            <v-icon>mdi-logout-variant</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>Выход</v-list-item-title>
           </v-list-item-content>
         </v-list-item>
       </v-list>
@@ -36,7 +63,7 @@
         <span class="mr-2">Личный кабинет</span>
         <v-icon>mdi-account</v-icon>
       </v-btn>
-    </v-app-bar> -->
+    </v-app-bar>-->
 
     <v-content>
       <router-view />
@@ -54,7 +81,26 @@ export default {
   data: () => ({
     drawer: true,
     drawerMiniVariant: true,
-    drawerExpandOnHover: true
+    drawerExpandOnHover: true,
+
+    // navigation
+    links: [
+      {
+        title: "Главная",
+        url: "/",
+        icon: "mdi-home"
+      },
+      {
+        title: "Новости",
+        url: "/news",
+        icon: "mdi-newspaper-variant"
+      },
+      {
+        title: "Курсы",
+        url: "/cources",
+        icon: "mdi-book-open-page-variant"
+      }
+    ]
   })
 };
 </script>
